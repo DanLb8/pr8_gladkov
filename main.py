6f065b6444498a6d1b4559edb4e52d323909aff2
Danil <danilrus58@yandex.ru>
2023-01-09 23:34:06 +0300

    add uppercase

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ s = ''
 #цикл на 11 элементов
 for i in range(11):
     #рандомно выбираем цифру из символов ASCII
-    s += random.choice(string.ascii_letters.lower())
+    s += random.choice(string.ascii_letters)
 print(s)
