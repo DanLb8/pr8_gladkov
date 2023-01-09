221a676d615fd5bdb0fadcc23635e99de81c97f5
Danil <danilrus58@yandex.ru>
2023-01-09 23:29:15 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#импорт библиотек
+import string
+import random
+#переменная для хранения выходной строки
+s = ''
+#цикл на 11 элементов
+for i in range(11):
+    #рандомно выбираем цифру из символов ASCII
+    s += random.choice(string.ascii_letters.lower())
+print(s)
