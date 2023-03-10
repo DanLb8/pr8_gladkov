01857810b1614b321b50f9e992c3efc4b7fcd887
DanLb8 <121770441+DanLb8@users.noreply.github.com>
2023-03-10 22:55:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-#импорт библиотек
-import string
+#импорт библиотеки
 import random
-#переменная для хранения выходной строки
-s = ''
-#цикл на 11 элементов
-for i in range(11):
-    #рандомно выбираем цифру из символов ASCII
-    s += random.choice(string.ascii_letters)
-<<<<<<< HEAD
-=======
-    s += str(random.randint(0, 9))
->>>>>>> dev
-print(s)
+#функция
+def password():
+    a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    d = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z']
+    g = ['1','2','3','4','5','6','7','8','9','0']
+    result = ''
+    for i in range(11):
+        result += random.choice(a+d+g)
+    return result
+p = password()
+print(p)
